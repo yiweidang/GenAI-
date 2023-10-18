ad5349505470456c3cce142100890d8840f69057
yiweidang <148301067+yiweidang@users.noreply.github.com>
2023-10-18 12:15:31 +0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,13 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('your_file.csv')  # Replace 'your_file.csv' with the actual file path
+df = pd.read_csv('your_file.csv')
 
-# Check if the 'Overdue' column exists in the DataFrame
-if 'Overdue' in df.columns:
-    # Filter the DataFrame to get only the overdue tasks
-    overdue_tasks = df[df['Overdue'] == True]
+# Filter tasks that are overdue
+overdue_tasks = df[df['Overdue'] == True]
 
-    # Get the total number of overdue tasks
-    total_overdue_tasks = len(overdue_tasks)
+# Get the total number of overdue tasks
+total_overdue_tasks = len(overdue_tasks)
 
-    print(f'Total number of tasks that are overdue: {total_overdue_tasks}')
-else:
-    print("The 'Overdue' column is not found in the CSV file.")
+# Print the total number of overdue tasks
+print(f"Total number of tasks that are overdue: {total_overdue_tasks}")
