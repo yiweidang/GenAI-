3be1cac9ecb526c39818772faae796606854a82b
yiweidang <148301067+yiweidang@users.noreply.github.com>
2023-10-18 23:43:16 +0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -2,21 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('Construction_Data_PM_Tasks_All_Projects.csv')
 
-# Filter for tasks that are overdue
-overdue_tasks = df[df['Overdue'] == True]
+# Function to generate bar chart for overdue tasks by project
+def generate_overdue_tasks_bar_chart():
+    overdue_tasks_by_project = df[df['OverDue'] == True].groupby('project').size()
+    overdue_tasks_by_project.plot(kind='bar', xlabel='Project ID', ylabel='Total Overdue Tasks', title='Overdue Tasks by Project')
+    plt.show()
 
-# Group overdue tasks by Project and count the number of tasks
-overdue_counts = overdue_tasks.groupby('Project').size()
-
-# Create a bar chart
-ax = overdue_counts.plot(kind='bar', figsize=(10, 6))
-
-# Set labels and title
-plt.xlabel('Project')
-plt.ylabel('Number of Overdue Tasks')
-plt.title('Total Number of Overdue Tasks by Project')
-
-# Show the plot
-plt.show()
+# Call the function to generate the bar chart
+generate_overdue_tasks_bar_chart()
