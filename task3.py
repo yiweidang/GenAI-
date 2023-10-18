29fac39917bd655e94687a8fa3c4627f0f8fa1c9
yiweidang <148301067+yiweidang@users.noreply.github.com>
2023-10-18 23:41:52 +0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,24 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('Construction_Data_PM_Tasks_All_Projects.csv')
 
-# Filter for tasks with Status 'Open' or 'Closed'
-filtered_df = df[df['Status'].isin(['Open', 'Closed'])]
+# Function to generate bar chart for Open and Closed tasks by task group
+def generate_open_closed_tasks_bar_chart():
+    # Filter the DataFrame for Open and Closed statuses
+    filtered_df = df[df['Status'].isin(['Open', 'Closed'])]
+    
+    grouped_data = filtered_df.groupby(['Task Group', 'Status']).size().unstack().fillna(0)
+    ax = grouped_data.plot(kind='bar', stacked=True)
+    ax.set_xlabel('Task Group')
+    ax.set_ylabel('Total Tasks')
+    ax.set_title('Open and Closed Tasks by Task Group')
+    plt.show()
 
-# Group tasks by Task Group and Status, and count the number of tasks
-task_counts = filtered_df.groupby(['Task Group', 'Status']).size().unstack(fill_value=0)
-
-# Create a bar chart
-ax = task_counts.plot(kind='bar', stacked=True, figsize=(10, 6))
-
-# Set labels and title
-plt.xlabel('Task Group')
-plt.ylabel('Number of Tasks')
-plt.title('Total Number of Open and Closed Tasks by Task Group')
-
-# Show the legend
-plt.legend(title='Status', loc='upper right')
-
-# Show the plot
-plt.show()
+# Call the function to generate the bar chart
+generate_open_closed_tasks_bar_chart()
