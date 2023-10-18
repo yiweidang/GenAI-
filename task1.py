403ab48d09df89dad47d3faa43997b69a9ec350f
yiweidang <148301067+yiweidang@users.noreply.github.com>
2023-10-18 23:40:24 +0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('Construction_Data_PM_Tasks_All_Projects.csv')
 
-# Filter tasks that are overdue
-overdue_tasks = df[df['Overdue'] == True]
+# Function to count the total number of overdue tasks
+def count_overdue_tasks():
+    overdue_tasks = df[df['OverDue'] == True]
+    total_overdue_tasks = len(overdue_tasks)
+    print(f"Total number of overdue tasks: {total_overdue_tasks}")
 
-# Get the total number of overdue tasks
-total_overdue_tasks = len(overdue_tasks)
-
-# Print the total number of overdue tasks
-print(f"Total number of tasks that are overdue: {total_overdue_tasks}")
+# Call the function to count overdue tasks
+count_overdue_tasks()
