26fe9040702eb2dc64be1505d753c7c582b4bc3e
yiweidang <148301067+yiweidang@users.noreply.github.com>
2023-10-18 12:18:00 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('your_file.csv')  # Replace 'your_file.csv' with the actual file path
+df = pd.read_csv('your_file.csv')
 
-# Check if the 'Status' and 'Task Group' columns exist in the DataFrame
-if 'Status' in df.columns and 'Task Group' in df.columns:
-    # Group the data by 'Task Group' and 'Status' and count the number of tasks in each group
-    task_group_counts = df.groupby(['Task Group', 'Status']).size().unstack(fill_value=0)
+# Filter for tasks with Status 'Open' or 'Closed'
+filtered_df = df[df['Status'].isin(['Open', 'Closed'])]
 
-    print(task_group_counts)
-else:
-    print("Either 'Status' or 'Task Group' columns are not found in the CSV file.")
+# Group tasks by Task Group and Status, and count the number of tasks
+task_counts = filtered_df.groupby(['Task Group', 'Status']).size().unstack(fill_value=0)
+
+# Print the result
+print("Total Number of Open and Closed Tasks by Task Group:")
+print(task_counts)
